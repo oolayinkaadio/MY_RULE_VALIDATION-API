9d5d1f55e04c5582f0b5a72ade00be009710c745
Olaosebikan <oolayinkaadio@gmail.com>
2021-01-30 06:21:08 +0100

    Initial commit

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ app.use(
 
 
 app.use('/api', dataRoutes);
-app.get('/', (req, res) => {
-    return res.send(200).json({
-        data: 'Welcome to the rule-validation-api'
-    });
-})
+// app.get('/', (req, res) => {
+//     return res.send(200).json({
+//         data: 'Welcome to the rule-validation-api'
+//     });
+// })
 
 // const port = process.env.PORT || 4000;
 
